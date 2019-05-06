1f51a19a003b541de73a941982a4d8ae7b2783db
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-05-06 12:27:34 +0300

    Updates

---
+++ precache-manifest.9914800831bc9aa429da17dd22946e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd8519591253277f67cdadaa3b06cf",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "da90a79d979c0e8a7051",
+    "url": "/test-project/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-project/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "da90a79d979c0e8a7051",
+    "url": "/test-project/static/js/main.c42e1cde.chunk.js"
+  },
+  {
+    "revision": "8c473920f916ba8f9ddf",
+    "url": "/test-project/static/js/runtime~main.8e57902c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
